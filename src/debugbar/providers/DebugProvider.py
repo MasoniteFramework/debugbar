d629fc83d5b6f425e18831bc416196359791c6b4
Joe Mancuso <idmann509@gmail.com>
2022-01-06 23:46:05 -0500

    added improvements to debugprovider

--- src/debugbar/providers/DebugProvider.py
+++ src/debugbar/providers/DebugProvider.py
@@ -1,11 +1,16 @@
 from masonite.providers import Provider
-from ..debugger import Debugger
+from ..Debugger import Debugger
 from ..collectors.MessageCollector import MessageCollector
 from ..collectors.PythonCollector import PythonCollector
 from ..collectors.QueryCollector import QueryCollector
 from ..collectors.KeyValueCollector import KeyValueCollector
 from ..collectors.MeasureCollector import MeasureCollector
-
+# from masonite.facades import Cache
+from masonite.utils.str import random_string  
+import json
+import time
+import glob
+import os
 
 class DebugProvider(Provider):
     def __init__(self, application):
@@ -18,13 +23,22 @@ class DebugProvider(Provider):
         debugger.add_collector(MessageCollector())
         debugger.add_collector(KeyValueCollector("Environment"))
         debugger.add_collector(time)
-        # debugger.add_collector(KeyValueCollector("Request"))
+        debugger.add_collector(KeyValueCollector("Request"))
         debugger.add_collector(QueryCollector().start_logging('masoniteorm.connection.queries'))
         self.application.bind('debugger', debugger)
 
     def boot(self):
         self.application.make('debugger').get_collector('Time').stop_measure('boot')
         response = self.application.make('response')
+        storage = self.application.make('storage')
         if 'text/html' in response.header('Content-Type'):
+            # Delete the contents of the directory first
+            files = glob.glob('storage/app/debug/*')
+            for f in files:
+                os.remove(f)
+
+            storage.disk('debug').put(f"x{str(time.time())+'-'+random_string(10)}.json", json.dumps(self.application.make('debugger').to_dict()))
             response.content += self.application.make('debugger').get_renderer('javascript').render()
             response.make_headers()
+        else:
+            storage.disk('debug').put(f"x{str(time.time())+'-'+random_string(10)}.json", json.dumps(self.application.make('debugger').to_dict()))
