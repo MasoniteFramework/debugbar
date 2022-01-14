628e267f159192159993e1e5d4ac6b03cf9e6f44
Joe Mancuso <idmann509@gmail.com>
2022-01-13 21:56:03 -0500

    fixed package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='python-debugbar',
+    name='masonite-debugbar',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
